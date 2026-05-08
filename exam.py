0fc38734450b48bc66f41777ac50e14d2af6970b
h.tate <h.tate@CSC-R246.local>
2026-05-08 13:27:27 +0900

    add

--- exam.py
+++ exam.py
@@ -4,6 +4,16 @@ B = list(map(int, input().split()))
 
 print(*map(lambda x, y: x - y, A, B))
 
+# # 別解
+# N = int(input())
+# A = list(map(int, input().split()))
+# B = list(map(int, input().split()))
+
+# answer = []
+# for i in range(N):
+#     answer.append(A[i] - B[i])
+# print(' '.join(map(str, answer)))
+
 # i = 0
 # while i < n:
 #     print(A[i] - B[i], end='')
